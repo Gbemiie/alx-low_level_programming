8ba973718458ecbeb0163990e6ba4816226d8fd2
Goodness Farayola <farayolagoodness@gmail.com>
2023-04-20 18:21:51 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints a line with puts function and end value of zero
+ * Return: 0 (positive)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
