32eb0013b8860f707aaed7714b80543181028d08
Goodness Farayola <farayolagoodness@gmail.com>
2023-04-22 19:38:18 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,11 +6,15 @@
  * main - Prints all single digit numbers of base 10 from 0, then a new line
  * Return: 0 (correct)
  */
+
 int main(void)
+
 {
+
 	int i;
+
 	for (i = '0'; i <= '9'; i++)
-		putchar(i);
+	putchar(i);
 	putchar('\n');
 	return (0);
 }
