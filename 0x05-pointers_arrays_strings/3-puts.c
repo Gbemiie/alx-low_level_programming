be6d7b07119a688bf7bc4fb862df1847edf32910
Goodness Farayola <farayolagoodness@gmail.com>
2023-05-02 18:54:30 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string,then a new line
+ * @str: string to be printed
+ * Return: 0 (correct)
+ */
+
+void _puts(char *str)
+
+{
+	while (*str != '\0')
+	{
+	_putchar(*str++);
+	}
+	_putschar('\n');
+}
