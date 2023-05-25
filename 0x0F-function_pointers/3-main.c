68c16ec14c7ad26735272f7885b3bfb043889d75
Goodness Farayola <farayolagoodness@gmail.com>
2023-05-24 20:32:43 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,35 +1,35 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "3.calc.h"
+#include "3-calc.h"
 /**
  * main - result of simple operations
  * @argc: arguments count
  * @argv: argument vector
  * Return: 0 (correct)
  */
-int main(int _attribute_((_unused_)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-int val1, val2;
+int num1, num2;
 char *op;
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-val1 = atoi(argv[1]);
+num1 = atoi(argv[1]);
 op = argv[2];
-val2 = atoi(argv[3]);
+num2 = atoi(argv[3]);
 if (get_op_func(op) == NULL || op[1] != '\0')
 {
 printf("Error\n");
 exit(99);
 }
-if ((*op == '/' && val2 == 0) || (*op == '%' && val2 == 0))
+if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 {
 printf("Error\n");
 exit(100);
 }
-printf("%d\n", get_op_func(op)(val1, val2));
+printf("%d\n", get_op_func(op)(num1, num2));
 return (0);
 }
